6c5be2e2e03a04a33aa099bc88a4e6bba131a181
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-12-10 21:15:23 +0000

    Added task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for a in range(0, 10):
+    for b in range(0, 10):
+        if b > a:
+            print("{:d}{:d}".format(a, b), end="")
+            if a < 8:
+                print("", end=", ")
+            else:
+                print("")
