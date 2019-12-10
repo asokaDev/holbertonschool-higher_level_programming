0a72c117590dd1b20313807c22251e83ee4c6176
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-12-10 21:41:39 +0000

    Added task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        ch = ord(str[i])
+        if ch > 96 and ch < 123:
+            ch -= 32
+        print("{:c}".format(ch), end="")
+    print()
