cf3dbdd440b2f17ffed4d20d6cc989ca300450fa
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-12-19 19:41:55 +0000

    Added task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,9 @@
 def uniq_add(my_list=[]):
     add = 0
     my_list.sort()
-    if my_list is not None or my_list is not []:
+    if my_list is None or my_list is []:
+        return 0
+    else:
         add += my_list[0]
     for i in range(1, len(my_list)):
         if my_list[i] is not my_list[i - 1]:
