fc03056ce48b2739136e5bb71ccd74eeaa5e2d73
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-01-09 04:46:39 +0000

    Added task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         for elem in range(0, x):
             print(my_list[elem], end="")
-            count += 1
+            cont += 1
         print()
         return cont
     except IndexError:
         print()
-        return count
+        return cont
