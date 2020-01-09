aa487ac52b2b474692f87845c8ead6ec5448680f
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-01-09 04:45:03 +0000

    Added task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    cont = 0
+    try:
+        for elem in range(0, x):
+            print(my_list[elem], end="")
+            count += 1
+        print()
+        return cont
+    except IndexError:
+        print()
+        return count
