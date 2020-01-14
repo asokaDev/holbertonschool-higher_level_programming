061c77b97c7e986cf99c30db9a7e9d8bc3e3a9b8
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-01-14 23:31:25 +0000

    Added task 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+    This module define a Rectangle and the operations that are possible
+    with this
+"""
+
+
+class Rectangle():
+    """Define a rectangle.
+
+    Args:
+        width: width of the rectangle (integer)
+        height:height of the rectangle (integer)
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get the width value"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width value"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height value"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Method that return the area of the square"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Method that return the area of the square"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width * 2) + (self.__height * 2)
