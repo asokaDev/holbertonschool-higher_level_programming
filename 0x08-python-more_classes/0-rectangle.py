56f96b2833422dbd7d103f89b3185083edb71538
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-01-14 23:30:51 +0000

    Added task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    This module define a Rectangle and the operations that are possible
+    with this
+"""
+
+
+class Rectangle():
+    """ Define a rectangle """
+    pass
