e8a44444ade12b85623935292b4c006278661b44
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-05-05 23:54:33 -0500

    Added task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const arg = process.argv;
+const len = process.argv.length;
+let sBig = 0;
+if (len > 3) {
+  arg.sort(function (a, b) { return a - b; });
+  sBig = arg[len - 2];
+}
+
+console.log(sBig);
