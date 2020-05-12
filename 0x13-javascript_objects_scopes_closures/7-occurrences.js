fcf1ea88a5664805d252e228fdb166c998f4368b
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-05-12 00:00:40 -0500

    Added task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,3 +1,11 @@
 #!/usr/bin/node
 
-
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (let i of list) {
+    if (i === searchElement) {
+      counter++;
+    }
+  }
+  return counter;
+};
