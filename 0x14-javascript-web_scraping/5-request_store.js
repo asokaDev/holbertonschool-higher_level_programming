46a1c14c4fe2bf543ca3a1aa10230276f7a59a8f
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-05-12 23:51:42 -0500

    Added task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,3 +1,15 @@
 #!/usr/bin/node
 
-
+const fs = require('fs');
+const request = require('request');
+request(process.argv[2], (error, res, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
