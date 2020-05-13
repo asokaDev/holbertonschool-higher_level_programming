d7cb21d990d5e4e59aad552279711fcd00feb754
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-05-12 23:57:08 -0500

    Added task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,3 +1,16 @@
 #!/usr/bin/node
 
-
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const arr = JSON.parse(body);
+    console.log(arr.reduce((obj, elem) => {
+      if (elem.completed) {
+        ret[elem.userId] ? ret[elem.userId]++ : ret[elem.userId] = 1;
+      }
+      return ret;
+    }, {}));
+  }
+});
