24df8c737cda9dd9f944fd9c7e3d140d4a303367
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-05-13 00:10:52 -0500

    Fixed style

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,8 +5,8 @@ request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (error, res, b
   if (error) {
     console.log(error);
   } else {
-    const characters  = JSON.parse(body).characters;
-    characters .forEach(char => {
+    const characters = JSON.parse(body).characters;
+    characters.forEach(char => {
       request(char, (error, res, body) => {
         if (error) {
           console.log(error);
