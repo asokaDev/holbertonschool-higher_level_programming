311b3f4302f7f18406909a30f3eb09109a3e69c4
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-08-18 23:58:12 -0500

    Added task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+# A Bash script that takes in a URL, sends a POST request to the passed URL
+curl --silent --request POST --data 'email=hr@holbertonschool.com&subject=I will always be here for PLD' "$1"
