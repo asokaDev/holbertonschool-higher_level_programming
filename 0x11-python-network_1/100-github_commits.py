58bc3595c9b7f8d5ca7f52d1eb7ae035bb41a32b
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-08-24 23:35:16 -0500

    Added task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" Interview challenge """
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    req = get("https://api.github.com/repos/{}/{}/commits".
+              format(argv[2], argv[1])).json()
+    num = 0
+    for val in req:
+        num += 1
+        name = val.get("commit").get("author").get("name")
+        sha = val.get("sha")
+        print("{}: {}".format(sha, name))
+        if num == 10:
+            break
