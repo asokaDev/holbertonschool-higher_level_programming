9ee8b2f45ce1e574f1dc1ffea29d103d5337334f
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-08-24 23:35:06 -0500

    Added task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""""""
+from sys import argv
+from requests import get, auth
+
+if __name__ == "__main__":
+    req = get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    print(req.json().get('id'))
