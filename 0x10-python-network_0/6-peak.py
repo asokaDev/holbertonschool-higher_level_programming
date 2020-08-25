6f2f397c284c80953176dcd8447a1475fd763891
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-08-24 23:59:28 -0500

    Added task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Function that finds a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """Function toind a peak"""
+    if not list_of_integers:
+        return None
+    return _find(list_of_integers, 0, len(list_of_integers) - 1,
+                 len(list_of_integers))
+
+
+def _find(arr, low, high, n):
+    """Auxiliar function"""
+    mid = low + (high - low)//2
+    if (mid == 0 or arr[mid - 1] <= arr[mid]) and\
+       (mid == n - 1 or arr[mid + 1] <= arr[mid]):
+        return arr[mid]
+    elif (mid > 0 and arr[mid - 1] > arr[mid]):
+        return _find(arr, low, mid - 1, n)
+    else:
+        return _find(arr, mid + 1, high, n)
