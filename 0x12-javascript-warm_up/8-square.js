07fbdf17369763f75a2131fae5b3c3c7f9047744
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-09-15 23:32:09 -0500

    Added task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+}
+for (let i = 0; i < process.argv[2]; i++) {
+  console.log('X'.repeat(parseInt(process.argv[2])));
+}
