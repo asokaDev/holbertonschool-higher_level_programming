ee01bcc372755d7674217940d748b31dd64ea6fb
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-09-22 00:00:37 -0500

    Added task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const SquareFather = require('./5-square');
+
+class Square extends SquareFather {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+}
+
+module.exports = Square;
