ce506afe6c5365531946f46c068b8c952e20da22
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-09-22 23:45:08 -0500

    Added task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    console.log(films.reduce((cnt, el) => {
+      el.characters.forEach(e => {
+        if (e.includes('18')) {
+          cnt++;
+        }
+      });
+      return cnt;
+    }, 0));
+  }
+});
