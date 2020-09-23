e2a1e77e973b0e5b3f7321cab391379e9c79a258
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-09-22 23:45:32 -0500

    Added task 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+const request = require('request');
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (error, res, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    characters.forEach(char => {
+      request(char, (error, res, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
